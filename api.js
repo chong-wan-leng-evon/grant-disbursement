68e2eb59b8d1e14ef95d9784e5adf3ddbfdac933
Evon-Chong <Evon.Chong@challenger.sg>
2022-06-29 11:54:35 +0800

    Update api.js

--- api.js
+++ api.js
@@ -332,4 +332,4 @@ app.delete('/delete-member/:household_family_id/:household_family_member_id', (r
 
 
 
-app.listen(3600, () => console.log('grant-disbursement api application is running'));
+app.listen(3000, () => console.log('grant-disbursement api application is running'));
